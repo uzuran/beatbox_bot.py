ced9a706c9b6e05cec5e85de1ecf5d757d8d6ae3
Uzuran <archloverlinux@gmail.com>
2020-11-20 21:35:17 +0100

    bot now have random speech

--- beatbox_Bot.py
+++ beatbox_Bot.py
@@ -1,3 +1,5 @@
+import os
+import random
 import discord
 import youtube_dl
 from discord.ext import commands
@@ -15,5 +17,25 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello! Check this out : https://www.facebook.com/czechbeatboxcommunity/ ')
+### random bot answer
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-client.run('Nzc4OTk2NTM2Mzk0NjQ1NTA0.X7aHDw.DV90LdPhJjo06kWI2hatjo63xQ8')
+    brooklyn_99_quotes = [
+        'Beatbox 💯 cool!!','Trénuj chlapče.','Možná ti to ukáže Shoter :robot:',
+        'Jooooo!!Tak to byla šupa.',
+        (
+            'Cože?Jak jsi to udělal? bumck cack clack crack?'
+            'Není to tak špatný no.Slyšel jsem že DeepLow to umí lépe.'
+        ),
+    ]
+
+    if message.content == 'beatbox' or 'Beatbox':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+client.run('')
